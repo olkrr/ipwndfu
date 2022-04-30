381777c39a4993ebf8cf2cef8ccd35c402d8625d
Olek <36710045+olkrr@users.noreply.github.com>
2022-04-30 17:42:22 +0200

    Update dfu.py

--- src/ipwndfu/dfu.py
+++ src/ipwndfu/dfu.py
@@ -38,7 +38,7 @@ def acquire_device(
     while not once or time.time() - start < timeout:
         once = True
         for device in usb.core.find(
-            find_all=True, device._langids = (1033,) idVendor=0x5AC, idProduct=0x1227, backend=backend, 
+            find_all=True, device._langids = (1033,), idVendor=0x5AC, idProduct=0x1227, backend=backend, 
         ):
             if match is not None and match not in device.serial_number:
                 continue
